5b2235082ad15ecd3a5b92f46fb6998f2df542e4
Olesia Shevchenko <olesia.shevchenko@nure.ua>
2025-01-01 23:53:38 +0200

    Create app.py

---
+++ Task3/arkpz-pzpi-22-8-shevchenko-olesia-task3/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from models import db
+from config import Config
+from mqtt_handler import start_mqtt_client
+from routes.users import users_bp
+from routes.labs import labs_bp
+from routes.access_events import access_events_bp
+from routes.sensors import sensors_bp
+from routes.admin_routes import admin_bp
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+app.register_blueprint(users_bp, url_prefix="/users")
+app.register_blueprint(labs_bp, url_prefix="/labs")
+app.register_blueprint(access_events_bp, url_prefix="/access_events")
+app.register_blueprint(sensors_bp, url_prefix="/sensors")
+app.register_blueprint(admin_bp, url_prefix="/admin")
+
+@app.route("/")
+def home():
+    return "API is working!"
